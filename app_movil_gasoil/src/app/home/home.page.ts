d06a774c7e93d7c11d4935ba9299903945673900
Dario Garcia <darcius_95@hotmail.com>
2020-12-03 13:56:59 +0100

    Creado formulario de insercion en el home
    Creado formulario, funcionando ya imprime por consola los valores introducidos de este.
    Falta terminar las validaciones del formulario, que funcionen e imprimir en pantalla los errores de cada campo

--- app_movil_gasoil/src/app/home/home.page.ts
+++ app_movil_gasoil/src/app/home/home.page.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormBuilder, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,45 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  insertForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.insertForm = this.formBuilder.group({
+      matricula: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("^[a-zA-Z]{1}[0-9]{4}[a-zA-Z]{2}$ || ^[0-9]{4}[a-zA-Z]{3}$"),
+        Validators.maxLength(7),
+      ])
+      ),
+      cantidad: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("^[0-9]+$")
+      ])
+      ),
+      kilometraje: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("^[0-9]+$")
+      ])
+      ),
+      empresa: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("^[a-zA-Z]+$")
+      ])
+      ),
+      obra: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("^[a-zA-Z0-9]+$")
+      ])
+      ),
+      notas: new FormControl("")
+    })
+  }
   
   ngOnInit() {
+  }
+
+  insertReg(datos){
+    console.log(datos);
   }
 
 
